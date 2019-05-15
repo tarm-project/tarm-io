3292063aea20a665d94f8b36cc0522670afb1abc
Alexander Voitenko <av@tcloud.pro>
2019-05-15 23:17:46 +0300

    Implemented basic text file transfer over the network

--- Server.cpp
+++ Server.cpp
@@ -11,18 +11,11 @@ int main(int argc, char* argv[]) {
     io::TcpServer server(loop);
 
     io::File file(loop);
-    file.open("cmake_install.cmake", [](io::File& file) {
-        std::cout << "Opened file " << file.path() << std::endl;
-
-        file.read([](io::File& file, const char* buf, std::size_t size) {
-            std::cout.write(buf, size);
-        });
-    });
 
     //io::Timer timer(loop);
     //timer.start([&](Timer& t){ std::cout << "Timer!!!" << std::endl; timer.stop();}, 1000, 0);
 
-    std::shared_ptr<char> write_data_buf(new char[64], std::default_delete<char[]>());
+    std::shared_ptr<char> write_data_buf(new char[io::File::READ_BUF_SIZE], std::default_delete<char[]>());
 
     auto on_new_connection = [&](const io::TcpServer& server, const io::TcpClient& client) -> bool {
         std::cout << "New connection from " << io::ip4_addr_to_string(client.ipv4_addr()) << ":" << client.port() << std::endl;
@@ -35,13 +28,29 @@ int main(int argc, char* argv[]) {
         return true;
     };
 
-    auto on_data_read = [&](const io::TcpServer&, const io::TcpClient&, const char* data, size_t len) {
+    auto on_data_read = [&](const io::TcpServer&, const io::TcpClient& client, const char* data, size_t len) {
         std::string message(data, data + len);
 
         if (message.find("close") != std::string::npos ) {
             std::cout << "Sutting down server..." << std::endl;
             server.shutdown();
             //timer.stop();
+        }
+
+        if (message.find("open") != std::string::npos ) {
+            auto pos = message.find("open");
+            auto file_name = message.substr(5, message.size() - 5 - 1); // TODO: magic values and strings
+
+            file.open(file_name, [&](io::File& file) {
+                std::cout << "Opened file " << file.path() << std::endl;
+
+                file.read([&](io::File& file, const char* buf, std::size_t size) {
+                    // TODO: this is wrong!!!
+                    io::TcpClient& client_ = const_cast<io::TcpClient&>(client);
+                    std::memcpy(write_data_buf.get(), buf, size);
+                    client_.send_data(write_data_buf, size);
+                });
+            });
         }
 
         std::cout << message;
