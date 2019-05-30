59991f22b2b7be4f7a190151d8a5f43e25eaa004
Alexander.Voitenko <av@tcloud.pro>
2019-05-30 20:11:45 +0300

    Added basic File test

--- tests/FileTest.cpp
+++ tests/FileTest.cpp
@@ -1,1 +1,15 @@
+#include "UTCommon.h"
+
 #include "io/File.h"
+
+struct FileTest : public testing::Test {
+
+};
+
+TEST_F(FileTest, default_constructor) {
+    io::EventLoop loop;
+
+    auto file = new io::File(loop);
+    ASSERT_FALSE(file->is_open());
+    file->schedule_removal();
+}
