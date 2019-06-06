395c239acb76ad3d1ebeba1a44f97c37e36883a2
Alexander.Voitenko <av@tcloud.pro>
2019-06-06 19:37:54 +0300

    Test: EventLoopTest::multiple_schedule_on_each_loop_cycle

--- tests/EventLoopTest.cpp
+++ tests/EventLoopTest.cpp
@@ -82,4 +82,42 @@ TEST_F(EventLoopTest, schedule_on_each_loop_cycle) {
     EXPECT_EQ(500, counter);
 }
 
-// TODO: multiple schedule_on_each_loop_cycle and stopping them not in order of creation
+TEST_F(EventLoopTest, multiple_schedule_on_each_loop_cycle) {
+    io::EventLoop loop;
+
+    size_t counter_1 = 0;
+    size_t handle_1 = 0;
+    size_t counter_2 = 0;
+    size_t handle_2 = 0;
+    size_t counter_3 = 0;
+    size_t handle_3 = 0;
+
+    handle_1 = loop.schedule_call_on_each_loop_cycle([&handle_1, &counter_1, &loop]() {
+        ++counter_1;
+
+        if (counter_1 == 500) {
+            loop.stop_call_on_each_loop_cycle(handle_1);
+        }
+    });
+
+    handle_2 = loop.schedule_call_on_each_loop_cycle([&handle_2, &counter_2, &loop]() {
+        ++counter_2;
+
+        if (counter_2 == 200) {
+            loop.stop_call_on_each_loop_cycle(handle_2);
+        }
+    });
+
+    handle_3 = loop.schedule_call_on_each_loop_cycle([&handle_3, &counter_3, &loop]() {
+        ++counter_3;
+
+        if (counter_3 == 300) {
+            loop.stop_call_on_each_loop_cycle(handle_3);
+        }
+    });
+
+    ASSERT_EQ(0, loop.run());
+    EXPECT_EQ(500, counter_1);
+    EXPECT_EQ(200, counter_2);
+    EXPECT_EQ(300, counter_3);
+}
