c3baa26ee91f6bb5db8b2e4f3ccc9bbdc4203c40
Alexander.Voitenko <av@tcloud.pro>
2019-06-10 19:44:37 +0300

    Performance: improved ip4_addr_to_string. Added tests

--- io/Common.cpp
+++ io/Common.cpp
@@ -11,25 +11,36 @@ void default_alloc_buffer(uv_handle_t* /*handle*/, size_t suggested_size, uv_buf
 
 namespace {
 
+void push_byte_as_str(std::string& s, std::uint8_t byte) {
+    if (byte >= 100) {
+        s.push_back(byte / 100 + '0');
+        s.push_back((byte % 100) / 10 + '0');
+        s.push_back((byte % 10) + '0');
+    } else if (byte >= 10) {
+        s.push_back((byte % 100) / 10 + '0');
+        s.push_back((byte % 10) + '0');
+    } else {
+        s.push_back((byte % 10) + '0');
+    }
+}
+
 } // namespace
 
+// This implementation works x8 times faster comparing to more sraightforward approach like
+// result += std::to_string((addr & 0xFF000000u) >> 24);
+// result += ".";
+// ...
 std::string ip4_addr_to_string(std::uint32_t addr) {
     std::string result;
 
-    const std::uint8_t addr_1 = (addr & 0xFF000000u) >> 24;
-    const std::uint8_t addr_2 = (addr & 0x00FF0000u) >> 16;
-    const std::uint8_t addr_3 = (addr & 0x0000FF00u) >> 8;
-    const std::uint8_t addr_4 = (addr & 0x000000FFu);
-
-    // TODO: measure performance of this. May try to use result.push_back insttead of += std::to_string(...
     result.reserve(16);
-    result += std::to_string(addr_1);
-    result += ".";
-    result += std::to_string(addr_2);
-    result += ".";
-    result += std::to_string(addr_3);
-    result += ".";
-    result += std::to_string(addr_4);
+    push_byte_as_str(result, (addr & 0xFF000000u) >> 24);
+    result.push_back('.');
+    push_byte_as_str(result, (addr & 0x00FF0000u) >> 16);
+    result.push_back('.');
+    push_byte_as_str(result, (addr & 0x0000FF00u) >> 8);
+    result.push_back('.');
+    push_byte_as_str(result, (addr & 0x000000FFu));
 
     return result;
 }
---
+++ tests/CommonTest.cpp
@@ -0,0 +1,36 @@
+#include "UTCommon.h"
+
+#include "io/Common.h"
+
+struct CommonTest : public testing::Test {
+};
+
+TEST_F(CommonTest, ip4_addr_single_conversion) {
+    EXPECT_EQ("255.127.7.1", io::ip4_addr_to_string(0xFF7F0701));
+}
+
+TEST_F(CommonTest, ip4_addr_batch_conversion) {
+    // Test description: just more complicated test to cover more values and cases
+    const std::size_t SIZE = 2000;
+
+    for (std::size_t i = 0; i < SIZE; ++i) {
+        const std::uint32_t addr = i * 2000000 + i;
+
+        const std::uint8_t addr_1 = (addr & 0xFF000000u) >> 24;
+        const std::uint8_t addr_2 = (addr & 0x00FF0000u) >> 16;
+        const std::uint8_t addr_3 = (addr & 0x0000FF00u) >> 8;
+        const std::uint8_t addr_4 = (addr & 0x000000FFu);
+
+        std::string expected;
+        expected += std::to_string(addr_1);
+        expected += ".";
+        expected += std::to_string(addr_2);
+        expected += ".";
+        expected += std::to_string(addr_3);
+        expected += ".";
+        expected += std::to_string(addr_4);
+
+        std::string actual = io::ip4_addr_to_string(addr);
+        ASSERT_EQ(expected, actual) << " i= " << i;
+    }
+}
