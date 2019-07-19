885060bb4e927f22f3c343055e8e9b0f04c28f9c
Alexander.Voitenko <av@tcloud.pro>
2019-07-19 20:42:54 +0300

    Refine: implemented PIMPL for TcpServer

--- io/TcpServer.cpp
+++ io/TcpServer.cpp
@@ -11,17 +11,70 @@
 
 namespace io {
 
-TcpServer::TcpServer(EventLoop& loop) :
+const size_t TcpServer::READ_BUFFER_SIZE;
+
+class TcpServer::Impl {
+public:
+    Impl(EventLoop& loop, TcpServer& parent);
+    ~Impl();
+
+    int bind(const std::string& ip_addr_str, std::uint16_t port);
+
+    int listen(NewConnectionCallback new_connection_callback,
+               DataReceivedCallback data_receive_callback,
+               int backlog_size = 128);
+
+    void shutdown();
+    void close();
+
+    std::size_t connected_clients_count() const;
+
+    void remove_client_connection(TcpClient* client);
+
+protected:
+    // statics
+    static void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
+
+    static void on_new_connection(uv_stream_t* server, int status);
+    static void on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf);
+
+    static void on_close(uv_handle_t* handle);
+    static void on_shutdown(uv_shutdown_t* req, int status);
+
+private:
+    TcpServer* m_parent;
+    EventLoop* m_loop;
+    uv_loop_t* m_uv_loop;
+
+    uv_tcp_t* m_server_handle = nullptr;
+
+    // TODO: most likely this data member is not need to be data member but some var on stack
+    struct sockaddr_in m_unix_addr;
+
+    NewConnectionCallback m_new_connection_callback = nullptr;
+    DataReceivedCallback m_data_receive_callback = nullptr;
+
+    // Using such interesting kind of mapping here to be able find connections by raw C pointer
+    // and also have benefits of RAII with unique_ptr
+    //std::map<uv_tcp_t*, TcpClientPtr> m_client_connections;
+    std::set<TcpClient*> m_client_connections;
+
+    // Made as unique_ptr because boost::pool has no move constructor defined
+    std::unique_ptr<boost::pool<>> m_pool;
+};
+
+TcpServer::Impl::Impl(EventLoop& loop, TcpServer& parent) :
+    m_parent(&parent),
     m_loop(&loop),
     m_uv_loop(reinterpret_cast<uv_loop_t*>(loop.raw_loop())),
     m_pool(std::make_unique<boost::pool<>>(TcpServer::READ_BUFFER_SIZE)) {
 }
 
-TcpServer::~TcpServer() {
+TcpServer::Impl::~Impl() {
     //shutdown();
 }
 
-int TcpServer::bind(const std::string& ip_addr_str, std::uint16_t port) {
+int TcpServer::Impl::bind(const std::string& ip_addr_str, std::uint16_t port) {
     m_server_handle = new uv_tcp_t;
     auto init_status = uv_tcp_init_ex(m_uv_loop, m_server_handle, AF_INET); // TODO: IPV6 support
     m_server_handle->data = this;
@@ -41,12 +94,12 @@ int TcpServer::bind(const std::string& ip_addr_str, std::uint16_t port) {
     return uv_tcp_bind(m_server_handle, reinterpret_cast<const struct sockaddr*>(&m_unix_addr), 0);
 }
 
-int TcpServer::listen(NewConnectionCallback new_connection_callback,
-                      DataReceivedCallback data_receive_callback,
-                      int backlog_size) {
+int TcpServer::Impl::listen(NewConnectionCallback new_connection_callback,
+                            DataReceivedCallback data_receive_callback,
+                            int backlog_size) {
     m_new_connection_callback = new_connection_callback;
     m_data_receive_callback = data_receive_callback;
-    int status = uv_listen(reinterpret_cast<uv_stream_t*>(m_server_handle), backlog_size, TcpServer::on_new_connection);
+    int status = uv_listen(reinterpret_cast<uv_stream_t*>(m_server_handle), backlog_size, on_new_connection);
 
     // TODO: instead of returning status code need to call TcpServer::on_new_connection with appropriate status set
     if (status < 0) {
@@ -55,7 +108,7 @@ int TcpServer::listen(NewConnectionCallback new_connection_callback,
     return status;
 }
 
-void TcpServer::shutdown() {
+void TcpServer::Impl::shutdown() {
     for (auto& client : m_client_connections) {
         client->shutdown(); // TODO: shutdown with schedule_removal?????
     }
@@ -72,37 +125,37 @@ void TcpServer::shutdown() {
     */
 
    if (m_server_handle && !uv_is_closing(reinterpret_cast<uv_handle_t*>(m_server_handle))) {
-        uv_close(reinterpret_cast<uv_handle_t*>(m_server_handle), TcpServer::on_close);
+        uv_close(reinterpret_cast<uv_handle_t*>(m_server_handle), on_close);
    }
 }
 
-void TcpServer::close() {
+void TcpServer::Impl::close() {
     // TODO: clients close??????
 
-    uv_close(reinterpret_cast<uv_handle_t*>(m_server_handle), TcpServer::on_close);
+    uv_close(reinterpret_cast<uv_handle_t*>(m_server_handle), on_close);
 }
 
-void TcpServer::remove_client_connection(TcpClient* client) {
+void TcpServer::Impl::remove_client_connection(TcpClient* client) {
     m_client_connections.erase(client);
     client->schedule_removal();
 }
 
-std::size_t TcpServer::connected_clients_count() const {
+std::size_t TcpServer::Impl::connected_clients_count() const {
     return m_client_connections.size();
 }
 
-// ////////////////////////////////////// static //////////////////////////////////////
+////////////////////////////////////////////// static //////////////////////////////////////////////
 
-void TcpServer::alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
+void TcpServer::Impl::alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
     // TODO: probably use suggested_size via handling hash table of different pools by block size
 
     auto& client = *reinterpret_cast<TcpClient*>(handle->data);
-    auto& this_ = client.server();
+    auto& this_ = *client.server().m_impl.get();
     buf->base = reinterpret_cast<char*>(this_.m_pool->malloc());
-    buf->len = this_.READ_BUFFER_SIZE;
+    buf->len = TcpServer::READ_BUFFER_SIZE;
 }
 
-void TcpServer::on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
+void TcpServer::Impl::on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf) {
     if (client->data == nullptr) {
         // TODO: error handling here
     }
@@ -111,11 +164,11 @@ void TcpServer::on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf)
     // buf
 
     auto& tcp_client = *reinterpret_cast<TcpClient*>(client->data);
-    auto& this_ = tcp_client.server();
+    auto& this_ = *tcp_client.server().m_impl.get();
 
     if (nread > 0) {
         if (this_.m_data_receive_callback) {
-            this_.m_data_receive_callback(this_, tcp_client, buf->base, nread);
+            this_.m_data_receive_callback(*this_.m_parent, tcp_client, buf->base, nread);
         }
         return;
     }
@@ -133,10 +186,10 @@ void TcpServer::on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf)
     this_.m_pool->free(buf->base);
 }
 
-void TcpServer::on_new_connection(uv_stream_t* server, int status) {
+void TcpServer::Impl::on_new_connection(uv_stream_t* server, int status) {
     assert(server && "server should be not null");
 
-    auto& this_ = *reinterpret_cast<TcpServer*>(server->data);
+    auto& this_ = *reinterpret_cast<TcpServer::Impl*>(server->data);
 
     this_.m_loop->log(Logger::Severity::TRACE, "TcpServer::on_new_connection");
 
@@ -148,7 +201,7 @@ void TcpServer::on_new_connection(uv_stream_t* server, int status) {
         return;
     }
 
-    auto tcp_client = new TcpClient(*this_.m_loop, this_);
+    auto tcp_client = new TcpClient(*this_.m_loop, *this_.m_parent);
 
     if (uv_accept(server, reinterpret_cast<uv_stream_t*>(tcp_client->tcp_client_stream())) == 0) {
         //sockaddr_storage info;
@@ -164,15 +217,15 @@ void TcpServer::on_new_connection(uv_stream_t* server, int status) {
 
             bool allow_connection = true;
             if (this_.m_new_connection_callback) {
-                allow_connection = this_.m_new_connection_callback(this_, *tcp_client);
+                allow_connection = this_.m_new_connection_callback(*this_.m_parent, *tcp_client);
             }
 
             if (allow_connection) {
                 this_.m_client_connections.insert(tcp_client);
 
                 uv_read_start(reinterpret_cast<uv_stream_t*>(tcp_client->tcp_client_stream()),
-                              TcpServer::alloc_buffer,
-                              TcpServer::on_read);
+                              alloc_buffer,
+                              on_read);
             } else {
                 // TODO: probably closing connection from the server side is not the best idea
                 // We can send message to client that server is not ready and disconnect from client side
@@ -191,16 +244,52 @@ void TcpServer::on_new_connection(uv_stream_t* server, int status) {
     }
 }
 
-void TcpServer::on_close(uv_handle_t* handle) {
-    auto& this_ = *reinterpret_cast<TcpServer*>(handle->data);
+void TcpServer::Impl::on_close(uv_handle_t* handle) {
+    auto& this_ = *reinterpret_cast<TcpServer::Impl*>(handle->data);
     this_.m_server_handle = nullptr;
     delete reinterpret_cast<uv_tcp_t*>(handle);
 }
 
 // TODO: candidate for removal
-void TcpServer::on_shutdown(uv_shutdown_t* req, int status) {
-    uv_close(reinterpret_cast<uv_handle_t*>(req->handle), TcpServer::on_close);
+void TcpServer::Impl::on_shutdown(uv_shutdown_t* req, int status) {
+    uv_close(reinterpret_cast<uv_handle_t*>(req->handle), on_close);
     delete req;
+}
+
+///////////////////////////////////////// implementation ///////////////////////////////////////////
+
+TcpServer::TcpServer(EventLoop& loop) :
+    /* Disposable(loop),*/
+    m_impl(new Impl(loop, *this)) {
+}
+
+TcpServer::~TcpServer() {
+}
+
+int TcpServer::bind(const std::string& ip_addr_str, std::uint16_t port) {
+    return m_impl->bind(ip_addr_str, port);
+}
+
+int TcpServer::listen(NewConnectionCallback new_connection_callback,
+                      DataReceivedCallback data_receive_callback,
+                      int backlog_size) {
+    return m_impl->listen(new_connection_callback, data_receive_callback, backlog_size);
+}
+
+void TcpServer::shutdown() {
+    return m_impl->shutdown();
+}
+
+void TcpServer::close() {
+    return m_impl->close();
+}
+
+std::size_t TcpServer::connected_clients_count() const {
+    return m_impl->connected_clients_count();
+}
+
+void TcpServer::remove_client_connection(TcpClient* client) {
+    return m_impl->remove_client_connection(client);
 }
 
 } // namespace io
--- io/TcpServer.h
+++ io/TcpServer.h
@@ -20,7 +20,7 @@ class TcpServer {
 public:
     friend class TcpClient;
 
-    const size_t READ_BUFFER_SIZE = 65536;
+    static const size_t READ_BUFFER_SIZE = 65536;
 
     // TODO: replace 'int status' with some error
     using NewConnectionCallback = std::function<bool(TcpServer&, TcpClient&)>;
@@ -49,35 +49,11 @@ public:
 
     std::size_t connected_clients_count() const;
 
-    // statics
-    static void alloc_buffer(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
-
-    static void on_new_connection(uv_stream_t* server, int status);
-    static void on_read(uv_stream_t* client, ssize_t nread, const uv_buf_t* buf);
-
-    static void on_close(uv_handle_t* handle);
-    static void on_shutdown(uv_shutdown_t* req, int status);
 private:
     void remove_client_connection(TcpClient* client);
 
-    EventLoop* m_loop;
-    uv_loop_t* m_uv_loop;
-
-    uv_tcp_t* m_server_handle = nullptr;
-
-    // TODO: most likely this data member is not need to be data member but some var on stack
-    struct sockaddr_in m_unix_addr;
-
-    NewConnectionCallback m_new_connection_callback = nullptr;
-    DataReceivedCallback m_data_receive_callback = nullptr;
-
-    // Using such interesting kind of mapping here to be able find connections by raw C pointer
-    // and also have benefits of RAII with unique_ptr
-    //std::map<uv_tcp_t*, TcpClientPtr> m_client_connections;
-    std::set<TcpClient*> m_client_connections;
-
-    // Made as unique_ptr because boost::pool has no move constructor defined
-    std::unique_ptr<boost::pool<>> m_pool;
+    class Impl;
+    std::unique_ptr<Impl> m_impl;
 };
 
 } // namespace io
