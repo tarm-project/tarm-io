a0d99a845a22bba1cc31568bcff99e2241af6fff
Alexander Voitenko <av@tcloud.pro>
2019-08-30 20:48:25 +0300

    Fixed length type mismatch for default_alloc_buffer

--- source/io/Common.cpp
+++ source/io/Common.cpp
@@ -4,7 +4,7 @@
 
 namespace io {
 
-void default_alloc_buffer(uv_handle_t* /*handle*/, size_t suggested_size, uv_buf_t* buf) {
+void default_alloc_buffer(uv_handle_t* /*handle*/, AllocSizeType suggested_size, uv_buf_t* buf) {
     buf->base = new char[suggested_size];
     buf->len = suggested_size;
 }
--- source/io/Common.h
+++ source/io/Common.h
@@ -6,6 +6,7 @@
 
 #include <uv.h>
 
+// TODO:
 #ifdef ERROR
     #undef ERROR
 #endif // ERROR
@@ -17,8 +18,10 @@
 
 namespace io {
 
+using AllocSizeType = decltype(uv_buf_t::len);
+
 IO_DLL_PUBLIC
-void default_alloc_buffer(uv_handle_t* /*handle*/, size_t suggested_size, uv_buf_t* buf);
+void default_alloc_buffer(uv_handle_t* /*handle*/, AllocSizeType suggested_size, uv_buf_t* buf);
 
 IO_DLL_PUBLIC
 std::string ip4_addr_to_string(std::uint32_t addr);
