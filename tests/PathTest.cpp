c6bde1e1f164f3810704743fa0bedd8d953d0529
Alexander.Voitenko <av@tcloud.pro>
2019-09-05 19:32:41 +0300

    Test: codecvt_argument for Path

--- tests/PathTest.cpp
+++ tests/PathTest.cpp
@@ -10,6 +10,8 @@
 
 #include <iterator>
 #include <string>
+#include <locale>
+#include <cwchar>  // for mbstate_t
 
 // Workaround for GTest printers bug and path
 // https://github.com/google/googlemock/issues/170
@@ -908,6 +910,142 @@ TEST_F(PathTest, imbue_locale) {
 
     path after("abc");
     EXPECT_TRUE(before == after);
+}
+
+namespace {
+
+#include <locale>
+#include <cwchar>  // for mbstate_t
+
+  //------------------------------------------------------------------------------------//
+  //                                                                                    //
+  //                              class test_codecvt                                    //
+  //                                                                                    //
+  //  Warning: partial implementation; even do_in and do_out only partially meet the    //
+  //  standard library specifications as the "to" buffer must hold the entire result.   //
+  //                                                                                    //
+  //  The value of a wide character is the value of the corresponding narrow character  //
+  //  plus 1. This ensures that compares against expected values will fail if the       //
+  //  code conversion did not occur as expected.                                        //
+  //                                                                                    //
+  //------------------------------------------------------------------------------------//
+
+class test_codecvt : public std::codecvt< wchar_t, char, std::mbstate_t> {
+public:
+    explicit test_codecvt()
+        : std::codecvt<wchar_t, char, std::mbstate_t>() {}
+
+protected:
+    virtual bool do_always_noconv() const throw() { return false; }
+    virtual int do_encoding() const throw() { return 0; }
+
+    virtual std::codecvt_base::result do_in(std::mbstate_t&,
+            const char* from, const char* from_end, const char*& from_next,
+            wchar_t* to, wchar_t* to_end, wchar_t*& to_next) const {
+        for (; from != from_end && to != to_end; ++from, ++to)
+            *to = wchar_t(*from + 1);
+
+        if (to == to_end)
+            return error;
+
+        *to = L'\0';
+        from_next = from;
+        to_next = to;
+        return ok;
+    }
+
+virtual std::codecvt_base::result do_out(std::mbstate_t&,
+            const wchar_t* from, const wchar_t* from_end, const wchar_t*& from_next,
+            char* to, char* to_end, char*& to_next) const {
+        for (; from != from_end && to != to_end; ++from, ++to)
+            *to = static_cast<char>(*from - 1);
+
+        if (to == to_end)
+            return error;
+
+        *to = '\0';
+        from_next = from;
+        to_next = to;
+        return ok;
+    }
+
+    virtual std::codecvt_base::result do_unshift(std::mbstate_t&,
+        char* /*from*/, char* /*to*/, char* & /*next*/) const  { return ok; }
+
+    virtual int do_length(std::mbstate_t&,
+        const char* /*from*/, const char* /*from_end*/, std::size_t /*max*/) const  { return 0; }
+
+    virtual int do_max_length() const throw () { return 0; }
+};
+
+// TODO: fixme
+# ifdef BOOST_WINDOWS_API
+  void check_native(const path& p,
+    const std::string&, const std::wstring& expected)
+# else
+  void check_native(const path& p,
+    const std::string& expected, const std::wstring&)
+# endif
+{
+    EXPECT_EQ(p.native(), expected);
+}
+
+} // namespace
+
+TEST_F(PathTest, codecvt_argument) {
+    const char * c1 = "a1";
+    const std::string s1(c1);
+    const std::wstring ws1(L"b2");  // off-by-one mimics test_codecvt
+    const std::string s2("y8");
+    const std::wstring ws2(L"z9");
+
+    test_codecvt cvt;  // produces off-by-one values that will always differ from
+                       // the system's default locale codecvt facet
+
+    //  constructors
+    path p(c1, cvt);
+    check_native(p, s1, ws1);
+
+    path p1(s1.begin(), s1.end(), cvt);
+    check_native(p1, s1, ws1);
+
+    path p2(ws2, cvt);
+    check_native(p2, s2, ws2);
+
+    path p3(ws2.begin(), ws2.end(), cvt);
+    check_native(p3, s2, ws2);
+
+    // path p2(p1, cvt);  // fails to compile, and that is OK
+
+    //  assigns
+    p1.clear();
+    p1.assign(s1,cvt);
+    check_native(p1, s1, ws1);
+    p1.clear();
+    p1.assign(s1.begin(), s1.end(), cvt);
+    check_native(p1, s1, ws1);
+    // p1.assign(p, cvt);  // fails to compile, and that is OK
+
+    //  appends
+    p1.clear();
+    p1.append(s1,cvt);
+    check_native(p1, s1, ws1);
+    p1.clear();
+    p1.append(s1.begin(), s1.end(), cvt);
+    check_native(p1, s1, ws1);
+    // p1.append(p, cvt);  // fails to compile, and that is OK
+
+    //  native observers
+    EXPECT_TRUE(p.string<std::string>(cvt) == s1);
+    EXPECT_TRUE(p.string(cvt) == s1);
+    EXPECT_TRUE(p.string<std::wstring>(cvt) == ws1);
+    EXPECT_TRUE(p.wstring(cvt) == ws1);
+
+    //  generic observers
+    EXPECT_TRUE(p.generic_string<std::string>(cvt) == s1);
+    EXPECT_TRUE(p.generic_string(cvt) == s1);
+    EXPECT_TRUE(p.generic_string<std::wstring>(cvt) == ws1);
+    EXPECT_TRUE(p.generic_wstring(cvt) == ws1);
 }
 
 TEST_F(PathTest, non_member) {
