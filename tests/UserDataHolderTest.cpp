1a10bb4b7165575363f965062932d5cf2f35c20b
Alexander Voitenko <av@tcloud.pro>
2019-12-01 12:35:34 +0300

    Move semantic tests for UserDataHolder

--- tests/UserDataHolderTest.cpp
+++ tests/UserDataHolderTest.cpp
@@ -2,6 +2,8 @@
 
 #include "io/UserDataHolder.h"
 
+#include <vector>
+
 struct UserDataHolderTest : public testing::Test,
                             public LogRedirector {
 
@@ -22,3 +24,24 @@ TEST_F(UserDataHolderTest, accessor) {
     ASSERT_EQ(&value, holder.user_data());
 }
 
+TEST_F(UserDataHolderTest, move_1) {
+    io::UserDataHolder holder;
+    holder.set_user_data(reinterpret_cast<void*>(42));
+
+    io::UserDataHolder holder_moved = std::move(holder);
+    EXPECT_EQ(42, reinterpret_cast<std::size_t>(holder_moved.user_data()));
+}
+
+TEST_F(UserDataHolderTest, move_2) {
+    const std::size_t COUNT = 1024;
+
+    std::vector<io::UserDataHolder> holders;
+    for (std::size_t i = 0; i < COUNT; ++i) {
+        holders.push_back(io::UserDataHolder());
+        holders.back().set_user_data(reinterpret_cast<void*>(i));
+    }
+
+    for (std::size_t i = 0; i < COUNT; ++i) {
+        EXPECT_EQ(i, reinterpret_cast<std::size_t>(holders[i].user_data()));
+    }
+}
