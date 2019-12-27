44d943c9620993302071c73e5fbbb45f51c66397
Alexander Voitenko <av@tcloud.pro>
2019-12-27 20:12:32 +0300

    Moved logic from header to cpp file  for LogRedirector

--- tests/LogRedirector.h
+++ tests/LogRedirector.h
@@ -2,51 +2,18 @@
 
 #include "io/global/Configuration.h"
 
-#include <gtest/gtest.h>
-
-#include <boost/filesystem.hpp>
-#include <boost/dll.hpp>
+#include <boost/filesystem/path.hpp>
 
 #include <fstream>
 #include <mutex>
-#include <assert.h>
 
-
-const char* const TEST_OUTPUT_PATH_RELATIVE = "../testlogs";
-const char* const LOG_EXTENSION = ".log";
-
-// TODO: PIMPL!!!!
+extern const char* const TEST_OUTPUT_PATH_RELATIVE;
 
 // LogRedirector class is used to redirect each test's logs into separate log files
 class LogRedirector {
 public:
-    LogRedirector(const std::string &log_path = TEST_OUTPUT_PATH_RELATIVE) noexcept {
-        const auto exec_dir =  boost::dll::program_location().parent_path();
-        m_logs_directory = exec_dir / log_path;
-
-        if (!boost::filesystem::exists(m_logs_directory)) {
-            boost::filesystem::create_directories(m_logs_directory);
-        }
-
-        const std::string current_test_scope = ::testing::UnitTest::GetInstance()->current_test_case()->name();
-        const std::string current_test_name = ::testing::UnitTest::GetInstance()->current_test_info()->name();
-
-        const auto full_path = m_logs_directory / (current_test_scope + "." + current_test_name + LOG_EXTENSION);
-        m_ofstream.open(full_path.string());
-        assert(!m_ofstream.fail());
-
-        io::global::set_logger_callback([this](const std::string& message) {
-            std::lock_guard<decltype(m_log_mutex)> guard(m_log_mutex);
-            m_ofstream << message << std::endl;
-        });
-    }
-
-    void log_to_stdout() {
-        io::global::set_logger_callback([this](const std::string& message) {
-            std::lock_guard<decltype(m_log_mutex)> guard(m_log_mutex);
-            std::cout << message << std::endl;
-        });
-    }
+    LogRedirector(const std::string& log_path = TEST_OUTPUT_PATH_RELATIVE) noexcept;
+    void log_to_stdout();
 
 private:
     boost::filesystem::path m_logs_directory;
