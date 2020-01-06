e0f25ad756bf8a2908482982cc6fceaa11fc2e10
Alexander Voitenko <av@tcloud.pro>
2020-01-06 19:42:41 +0300

    Removed -openssl1.0.2k Docker file. Fixed build_all_images.bash script to not include template docker files

--- tests/docker/build_all_images.bash
+++ tests/docker/build_all_images.bash
@@ -8,7 +8,7 @@ SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
 
 IFS=$'\n'
-DOCKER_FILES=( $(find ${SCRIPT_DIR} -type f -not -executable | sort) )
+DOCKER_FILES=( $(find ${SCRIPT_DIR} -type f -not -executable -and -not -name "*.template" | sort) )
 
 for i in "${DOCKER_FILES[@]}"; do
     echo Building image "$i"
