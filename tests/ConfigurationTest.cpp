8f0c77c7c7d1a6cc4813e53e9e563c48c65d30d7
Alexander Voitenko <av@tcloud.pro>
2020-03-09 20:22:49 +0300

    Implemented very first version of probing for send/receive socket buffers. Work in progress

---
+++ tests/ConfigurationTest.cpp
@@ -0,0 +1,13 @@
+#include "UTCommon.h"
+
+#include "io/global/Configuration.h"
+
+struct ConfigurationTest : public testing::Test,
+                           public LogRedirector {
+};
+
+TEST_F(ConfigurationTest, receive_buffer_size) {
+    std::cout << io::global::min_receive_buffer_size() << std::endl;
+    std::cout << io::global::default_receive_buffer_size() << std::endl;
+    std::cout << io::global::max_receive_buffer_size() << std::endl;
+}
