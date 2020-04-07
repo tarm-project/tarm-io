8e7e3b846719f9a6105fc87ccc30bfc9c63ac972
Alexander Voitenko <av@tcloud.pro>
2020-04-07 21:44:58 +0300

    Removed test client_set_minimal_buffer_size_and_send_more for UDP

--- tests/UdpClientServerTest.cpp
+++ tests/UdpClientServerTest.cpp
@@ -448,53 +448,6 @@ TEST_F(UdpClientServerTest, set_minimal_buffer_size) {
     EXPECT_EQ(1, client_on_receive_counter);
 }
 
-// TODO:
-TEST_F(UdpClientServerTest, DISABLED_client_set_minimal_buffer_size_and_send_more) {
-    io::EventLoop loop;
-
-    auto server = new io::UdpServer(loop);
-    auto listen_error = server->start_receive({m_default_addr, m_default_port},
-        [&](io::UdpPeer& peer, const io::DataChunk& data, const io::Error& error) {
-            EXPECT_FALSE(error);
-            std::cout << "!!!!" << std::endl;
-        }
-    );
-    EXPECT_FALSE(listen_error);
-
-
-    auto client = new io::UdpClient(loop);
-    EXPECT_FALSE(client->set_destination({0x7F000001u, m_default_port}));
-
-    const auto min_send_buffer_size = io::global::min_send_buffer_size();
-    //EXPECT_FALSE(client->set_send_buffer_size(min_send_buffer_size));
-    //EXPECT_FALSE(client->set_receive_buffer_size(min_send_buffer_size));
-
-    //EXPECT_FALSE(server->set_send_buffer_size(min_send_buffer_size));
-    EXPECT_FALSE(server->set_receive_buffer_size(min_send_buffer_size));
-
-    const std::size_t buf_size = min_send_buffer_size + 1;
-    std::shared_ptr<char> buf(new char[buf_size], std::default_delete<char[]>());
-    std::memset(buf.get(), 0, buf_size);
-
-    client->send_data(buf, buf_size,
-        [](io::UdpClient& peer, const io::Error& error) {
-        EXPECT_FALSE(error) << error.string();
-    });
-    client->send_data(buf, buf_size,
-        [](io::UdpClient& peer, const io::Error& error) {
-        EXPECT_FALSE(error) << error.string();
-    });
-    client->send_data(buf, buf_size,
-        [](io::UdpClient& peer, const io::Error& error) {
-        EXPECT_FALSE(error) << error.string();
-    });
-
-
-    //client->schedule_removal();
-
-    ASSERT_EQ(0, loop.run());
-}
-
 TEST_F(UdpClientServerTest, peer_identity_without_preservation_on_server) {
     io::EventLoop loop;
 
