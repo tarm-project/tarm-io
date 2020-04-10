098eabf24a34a9c9b920ad29ccc9aff8f83ba2cc
Alexander Voitenko <av@tcloud.pro>
2020-04-10 21:04:14 +0300

    Minor comment update

--- source/io/Convert.h
+++ source/io/Convert.h
@@ -14,4 +14,6 @@ std::string ip4_addr_to_string(std::uint32_t addr);
 IO_DLL_PUBLIC
 Error string_to_ip4_addr(const std::string& string_address, std::uint32_t& uint_address);
 
+// TOOD: ipv6??????
+
 } // namespace io
