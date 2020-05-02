0a8b3278718cb3d504a268fc127a3f9f47ce065f
Alexander Voitenko <av@tarm.io>
2020-05-02 22:05:12 +0300

    Added simple watchdog for hanged tests abort with 5 minutes timeout

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -11,13 +11,49 @@
     #include <openssl/applink.c>
 #endif
 
+#include <atomic>
 #include <iostream>
+#include <thread>
+
+std::atomic<bool> stop_watchdog(false);
+
+void hanged_tests_watchdog() {
+    const auto LIMIT = std::chrono::minutes(5);
+
+    auto seconds_counter = std::chrono::seconds(0);
+    while(!stop_watchdog) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        seconds_counter += std::chrono::seconds(1);
+
+        if (seconds_counter >= LIMIT) {
+            break;
+        }
+    }
+
+    if (stop_watchdog) {
+        return;
+    }
+
+    const ::testing::TestInfo* const test_info = ::testing::UnitTest::GetInstance()->current_test_info();
+    if (test_info) {
+        std::cout << "Total tests execution time is over limit of "  <<
+            std::chrono::duration_cast<std::chrono::seconds>(LIMIT).count() <<
+            " seconds, aborting. Last test: " << test_info->test_suite_name() << "." << test_info->name() << std::endl;
+    }
+
+    abort();
+}
 
 int main(int argc, char **argv) {
     io::global::set_logger_callback([](const std::string& message) {
         std::cout << message << std::endl;
     });
 
+    std::thread watch_dog(hanged_tests_watchdog);
+
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    const auto result = RUN_ALL_TESTS();
+    stop_watchdog = true;
+    watch_dog.join();
+    return result;
 }
