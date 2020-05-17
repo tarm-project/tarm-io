4920b8e8d7c05e4d752de47d931bc67970eccd56
Alexander Voitenko <av@tarm.io>
2020-05-17 11:32:06 +0300

    Overriden some Wild theme style in docs

--- doc/conf.py
+++ doc/conf.py
@@ -57,3 +57,9 @@ html_logo = 'static/logo_200px.png'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
+
+html_style = 'custom.css'
+
+#html_css_files = [
+#    'static/custom.css'
+#]
