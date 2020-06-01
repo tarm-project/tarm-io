9960a40f098efb7fba02d1b403ddbedc3546cd6e
Alexander Voitenko <av@tarm.io>
2020-06-01 18:35:59 +0300

    docker: added alpine Linxu images

--- scripts/execute_in_docker.bash
+++ scripts/execute_in_docker.bash
@@ -30,6 +30,21 @@ fi
 # Domain socket (needed for tests)
 CREATE_DOMAIN_SOCKET_COMMAND="python -c \"import socket as s; sock = s.socket(s.AF_UNIX); sock.bind('/var/run/somesocket')\""
 
+#CREATE_GROUP="groupadd -g ${GROUP_ID} ${USER}"
+#CREATE_USER="useradd -u ${USER_ID} -g ${USER} --create-home -s /bin/bash ${USER}"
+
+if [[ ${IMAGE_TO_EXECUTE} == *"alpine"* ]]; then
+    echo "Alpine Linux detected!"
+    CREATE_GROUP="true" 
+    CREATE_USER="adduser --disabled-password --uid ${USER_ID} --shell /bin/bash -g ${USER} ${USER}"
+else
+    CREATE_GROUP="groupadd -g ${GROUP_ID} ${USER}"
+    CREATE_USER="useradd -u ${USER_ID} -g ${USER} --create-home -s /bin/bash ${USER}"
+fi
+
+#CREATE_GROUP="addgroup -S ${USER}"
+#CREATE_USER="adduser --shell /bin/bash -S -G ${USER} ${USER}"
+
 # '--privileged' is required for leak sanitizer(and other tools) to work.
 docker run ${DOCKER_TERMINAL_OPTIONS} \
            --privileged \
@@ -41,8 +56,8 @@ docker run ${DOCKER_TERMINAL_OPTIONS} \
            bash -c "echo 'You are in docker now!' && \
                     ${CREATE_DOMAIN_SOCKET_COMMAND} && \
                     export HOME=/home/${USER} && \
-                    groupadd -g ${GROUP_ID} ${USER} && \
-                    useradd -u ${USER_ID} -g ${USER} --create-home -s /bin/bash ${USER} && \
+                    ${CREATE_GROUP} && \
+                    ${CREATE_USER} && \
                     echo '${USER}   ALL = NOPASSWD : ALL' | sudo EDITOR='tee -a' visudo > /dev/null && \
                     sudo PATH=\$PATH -E -u ${USER} bash -c \"${COMMAND_TO_EXECUTE}\""
 
