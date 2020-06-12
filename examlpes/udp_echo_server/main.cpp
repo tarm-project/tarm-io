bd5a0a322bcf9faa51803f4172b18babbf3bbf29
Alexander Voitenko <av@tarm.io>
2020-06-12 17:50:07 +0300

    Examples: added signal handler to UDP echo server example

--- examlpes/udp_echo_server/main.cpp
+++ examlpes/udp_echo_server/main.cpp
@@ -1,3 +1,11 @@
+/*----------------------------------------------------------------------------------------------
+ *  Copyright (c) 2020 - present Alexander Voitenko
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *----------------------------------------------------------------------------------------------*/
+
+// Important notice: if you change this code, ensure that docs are displaying it correctly,
+//                   because lines positions are important.
+
 #include <tarm/io/UdpServer.h>
 
 #include <iostream>
@@ -5,30 +13,46 @@
 using namespace tarm;
 
 int main() {
-   io::EventLoop loop;
+    io::EventLoop loop;
 
-   const auto port = 1234;
+    const auto port = 1234;
 
-   auto server = new io::UdpServer(loop);
-   auto listen_error = server->start_receive(
-      {"0.0.0.0", port},
-      [&](io::UdpPeer& peer, const io::DataChunk& data, const io::Error& error) {
+    auto server = new io::UdpServer(loop);
+    auto listen_error = server->start_receive(
+        {"0.0.0.0", port},
+        [&](io::UdpPeer& peer, const io::DataChunk& data, const io::Error& error) {
             if (error) {
-               std::cerr << "Error: " << error << std::endl;
-               return;
+                std::cerr << "Data receive error: " << error << std::endl;
+                return;
+            }
+            peer.send_data(std::string(data.buf.get(), data.size));
+        }
+    );
+
+    if (listen_error) {
+        std::cerr << "Listen error: " << listen_error << std::endl;
+        return 1;
+    }
+
+    std::cout << "Started listening on port " << port << std::endl;
+
+    const auto signal_handler_error = loop.handle_signal_once(
+        io::EventLoop::Signal::INT,
+        [&](io::EventLoop&, const io::Error& error) {
+            if (error) {
+                std::cerr << "Signal handling error: " << error << std::endl;
+                return;
             }
 
-            peer.send_data(std::string(data.buf.get(), data.size));
-      }
-   );
+            std::cout << std::endl << "Shutting down..." << std::endl;
+            server->schedule_removal();
+        }
+    );
 
-   if (listen_error) {
-      std::cerr << "Error: " << listen_error << std::endl;
-      return 1;
-   }
+    if (signal_handler_error) {
+        std::cerr << "Signal handling error: " << signal_handler_error << std::endl;
+        return 1;
+    }
 
-   std::cout << "Started listening on port " << port << std::endl;
-
-   // TODO: signal handler and server deletion
-   return loop.run();
+    return loop.run();
 }
