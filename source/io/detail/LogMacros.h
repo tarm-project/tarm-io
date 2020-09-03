165502564b085cf85516ce17cebed5dca8ef7696
Alexander Voitenko <av@tarm.io>
2020-09-03 18:05:39 +0300

    Log: code cleanup

--- source/io/detail/LogMacros.h
+++ source/io/detail/LogMacros.h
@@ -1,10 +1,5 @@
 #pragma once
 
-// TODO: remove
-//#define LOG_SEVERITY(LOGGER_PTR, ...) { \
-//    constexpr const char* file_name = ::tarm::io::detail::extract_file_name_from_path(__FILE__); \
-//    (LOGGER_PTR)->log_with_compile_context(::tarm::io::Logger::Severity::SEVERITY, file_name, __LINE__, __func__, __VA_ARGS__);}
-
 // No TARM_IO_ prefix because these macros are purely internal and not exposed
 #define LOG_IMPL(LOGGER_PTR, SEVERITY, ...) { \
     constexpr const char* file_name = ::tarm::io::detail::extract_file_name_from_path(__FILE__); \
