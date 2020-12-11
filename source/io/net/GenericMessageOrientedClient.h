6c39930d33c53064e7b24b248bfb8df1f169474a
Alexander Voitenko <av@tarm.io>
2020-12-11 22:17:48 +0300

    Net: Fixed size send in GenericMessageOrientedClient
    to not use stack memory because it can lead to a garbled data

--- source/io/net/GenericMessageOrientedClient.h
+++ source/io/net/GenericMessageOrientedClient.h
@@ -51,57 +51,7 @@ public:
                 }
             },
             [=](ClientType&, const DataChunk& data, const Error& error) {
-                if (error) {
-                    // TODO: pass error to receive_callback
-                    return;
-                }
-
-                std::size_t bytes_left = data.size;
-                std::size_t bytes_processed = data.size - bytes_left;
-                while (bytes_left) {
-                    if (!m_current_message_size.is_complete()) {
-                        bytes_processed = data.size - bytes_left;
-                        const auto size_bytes = m_current_message_size.add_bytes(
-                            reinterpret_cast<const std::uint8_t*>(data.buf.get() + bytes_processed), bytes_left);
-                        if (size_bytes > bytes_left) {
-                            // TODO: error handling here
-                            // This could happen in case of corrupted stream
-                            return;
-                        }
-
-                        if (current_message_too_large()) {
-                            if (receive_callback) {
-                                receive_callback(*this, {nullptr, static_cast<std::size_t>(m_current_message_size.value())}, StatusCode::MESSAGE_TOO_LONG);
-                            }
-                        }
-
-                        bytes_left -= size_bytes;
-                        if (bytes_left == 0) {
-                            return;
-                        }
-                    }
-
-                    bytes_processed = data.size - bytes_left;
-
-                    const std::size_t size_to_copy = (m_current_message_size.value() - m_current_message_offset) < bytes_left ?
-                                                     (m_current_message_size.value() - m_current_message_offset) :
-                                                     bytes_left;
-
-                    if (!current_message_too_large()) {
-                        std::memcpy(m_buffer.get() + m_current_message_offset, data.buf.get() + bytes_processed, size_to_copy);
-                    }
-                    m_current_message_offset += size_to_copy;
-                    bytes_left -= size_to_copy;
-
-                    if (m_current_message_offset == m_current_message_size.value()) {
-                        if (receive_callback && !current_message_too_large())  {
-                            receive_callback(*this, {m_buffer, static_cast<std::size_t>(m_current_message_size.value())}, error);
-                            // TODO: chaeck use count of that shared ptr and allocate new buffer if required
-                        }
-                        m_current_message_size.reset();
-                        m_current_message_offset = 0;
-                    }
-                }
+                on_data_receive(receive_callback, data, error);
             },
             [=](ClientType&, const Error& error) {
                 if (close_callback) {
@@ -112,14 +62,69 @@ public:
         );
     }
 
-    void send_data(const char* c_str, std::uint32_t size, const EndSendCallback& callback = nullptr) {
-        ::tarm::io::detail::VariableLengthSize vs(size);
-        if (vs.is_complete() && vs.bytes_count()) {
-            m_client->send_data(reinterpret_cast<const char*>(vs.bytes()), vs.bytes_count());
-            m_client->send_data(c_str, size);
-        } else {
-            // TODO: error
+    template<typename ReceiveCallback>
+    void on_data_receive(ReceiveCallback& receive_callback, const DataChunk& data, const Error& error) {
+        if (error) {
+            // TODO: pass error to receive_callback
+            return;
         }
+
+        std::size_t bytes_left = data.size;
+        std::size_t bytes_processed = data.size - bytes_left;
+        while (bytes_left) {
+            if (!m_current_message_size.is_complete()) {
+                bytes_processed = data.size - bytes_left;
+                const auto size_bytes = m_current_message_size.add_bytes(
+                    reinterpret_cast<const std::uint8_t*>(data.buf.get() + bytes_processed), bytes_left);
+                if (size_bytes > bytes_left) {
+                    // TODO: error handling here
+                    // This could happen in case of corrupted stream
+                    return;
+                }
+
+                if (current_message_too_large()) {
+                    if (receive_callback) {
+                        receive_callback(*this, {nullptr, static_cast<std::size_t>(m_current_message_size.value())}, StatusCode::MESSAGE_TOO_LONG);
+                    }
+                }
+
+                bytes_left -= size_bytes;
+                if (bytes_left == 0) {
+                    return;
+                }
+            }
+
+            bytes_processed = data.size - bytes_left;
+
+            const std::size_t size_to_copy = (m_current_message_size.value() - m_current_message_offset) < bytes_left ?
+                                             (m_current_message_size.value() - m_current_message_offset) :
+                                             bytes_left;
+
+            if (!current_message_too_large()) {
+                std::memcpy(m_buffer.get() + m_current_message_offset, data.buf.get() + bytes_processed, size_to_copy);
+            }
+            m_current_message_offset += size_to_copy;
+            bytes_left -= size_to_copy;
+
+            if (m_current_message_offset == m_current_message_size.value()) {
+                if (receive_callback && !current_message_too_large())  {
+                    receive_callback(*this, {m_buffer, static_cast<std::size_t>(m_current_message_size.value())}, error);
+                    // TODO: chaeck use count of that shared ptr and allocate new buffer if required
+                }
+                m_current_message_size.reset();
+                m_current_message_offset = 0;
+            }
+        }
+    }
+
+    void send_data(const char* c_str, std::uint32_t size, const EndSendCallback& callback = nullptr) {
+        do_send_size(size);
+        //if (vs.is_complete() && vs.bytes_count()) {
+            //m_client->send_data(reinterpret_cast<const char*>(vs.bytes()), vs.bytes_count());
+            m_client->send_data(c_str, size);
+        //} else {
+            // TODO: error
+        //}
     }
 
     void send_data(std::shared_ptr<const char> buffer, std::uint32_t size, const EndSendCallback& callback = nullptr);
@@ -137,6 +142,16 @@ public:
 private:
     bool current_message_too_large() const {
         return m_current_message_size.is_complete() && m_current_message_size.value() > m_max_message_size;
+    }
+
+    template<typename SizeType>
+    void do_send_size(SizeType size) {
+        // TODO: handle zero size
+        ::tarm::io::detail::VariableLengthSize vs(size);
+        std::shared_ptr<char> buf(new  char[vs.bytes_count()], std::default_delete<char[]>());
+        std::memcpy(buf.get(), vs.bytes(), vs.bytes_count());
+        // TODO: handle send error  ???
+        m_client->send_data(buf, vs.bytes_count());
     }
 
     std::size_t m_max_message_size;
